57a081e615d7ebc5a59f5d5736cb62bc160a44f6
unknown <arthurrgspinto@gmail.com>
2025-05-05 16:18:36 -0300

    Começo da resolução exercício 2

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-let numU;
+let soma = 0;
+let i = 1;
+let input = 0;
 
 do {
-    numU = parseInt(prompt("Digite um número:"));
-    if (numU < 0 || isNaN(numU)) {
-        alert("Você inseriu um valor inválido.");
+    input = parseFloat(prompt(`Insira a sua nota ${i}.`));
+    if (input > 10 || isNaN(input)) {
+        alert("Você inseriu um valor inválido. Tente inserir um número real, entre 0 e 10")
+    } else if (input < 0) {
+        alert("Você optou por parar com as somas da nota de suas provas.")
+    } else {
+        i = i + 1;
+        soma = soma + input;
     }
-} while (numU < 0 || isNaN(numU))
+} while (input > 0 || isNaN(input))
+
+alert(`A médias das suas ${(i - 2)} notas foi de ${(soma / (i - 1)).toFixed(2)}`);
