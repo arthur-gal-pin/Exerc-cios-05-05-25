47746668c15e79f8b4813ff07ed9a49228b51cd4
unknown <arthurrgspinto@gmail.com>
2025-05-08 09:26:04 -0300

    Exercício 3 finalizado e testado

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-let soma = 0;
-let i = 1;
+let valor = 0;
+let i = 0;
 let input = 0;
 
+alert("Olá! Seja bem-vindo ao sistema de cálculo de compras online.")
 do {
-    input = parseFloat(prompt(`Insira a sua nota ${i}.`));
-    if (input > 10 || isNaN(input)) {
-        alert("Você inseriu um valor inválido. Tente inserir um número real, entre 0 e 10")
-    } else if (input < 0) {
-        alert("Você optou por parar com as somas da nota de suas provas.")
+    input = parseFloat(prompt(`Insira o valor do seu ${i+1}º produto.`));
+    if (isNaN(input) || input < 0) {
+        alert("Você inseriu um valor inválido. Tente inserir um número real positivo.")
+    } else if (input === 0) {
+        alert("Você optou por parar com as somas das suas compras.")
     } else {
         i = i + 1;
-        soma = soma + input;
+        valor = valor + input;
     }
 } while (input > 0 || isNaN(input))
 
-alert(`A médias das suas ${(i - 2)} notas foi de ${(soma / (i - 1)).toFixed(2)}`);
+alert(`A médias das suas ${i} compras foi de R$${valor}`);
