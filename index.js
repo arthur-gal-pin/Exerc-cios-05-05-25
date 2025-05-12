28f33e0d17aa1a336a84fb87490dfc0221b94d27
unknown <arthurrgspinto@gmail.com>
2025-05-12 13:25:05 -0300

    Exercício 2 revisado e comentado.

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-let soma = 0;
-let i = 0;
-let input = 0;
+let soma = 0; //Representa a soma de todas as notas inseridas pelo usuário.
+let i = 0; //Conta a quantidade de notas já inseridas, para calcular a média posteriormente.
+let input = 0; //Nota inserida pelo aluno no código.
 
 do {
-    input = parseFloat(prompt(`Insira a sua nota ${i+1}.`));
+    input = (prompt(`Insira a sua nota ${i+1}. Use ponto para expressar as vírgulas.`)); //Recebe o input
     if (input > 10 || isNaN(input)) {
-        alert("Você inseriu um valor inválido. Tente inserir um número racional, entre 0 e 10")
+        alert("Você inseriu um valor inválido. Tente inserir um número racional, entre 0 e 10."); //Verifica a validade do input.
     } else if (input < 0) {
-        alert("Você optou por parar com as somas da nota de suas provas.")
+        alert("Você optou por parar com as somas da nota de suas provas."); //Verifica e anuncia o fim do programa.
     } else {
-        i = i + 1;
-        soma = soma + input;
+        input = parseFloat(input);
+        i ++;
+        soma += input; //Realiza a soma das notas.
+        console.log(soma, i)
     }
 } while (input > 0 || isNaN(input))
 
-alert(`A médias das suas ${(i)} notas foi de ${(soma / (i)).toFixed(2)}`);
+alert(`A médias das suas ${(i)} notas foi de ${(soma / (i)).toFixed(2)}`); //Devolve o output, e realiza o cálculo das médias.
