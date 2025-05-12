c5ba35ce1fad24b87cdfa99074767e8a1d40ef86
unknown <arthurrgspinto@gmail.com>
2025-05-12 12:53:53 -0300

    Exercício 1 revisado e comentado.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let numU;
 
 do {
-    numU = parseInt(prompt("Digite um número:"));
-    if (numU < 0 || isNaN(numU)) {
+    numU = parseInt(prompt("Digite um número positivo:")); //Variável é um número inteiro, que precisa ser positivo.
+    if (numU <= 0 || isNaN(numU)) { //Condição que verfica se o input é válido.
         alert("Você inseriu um valor inválido.");
     }
-} while (numU < 0 || isNaN(numU))
+} while (numU <= 0 || isNaN(numU)) //Repete a condição até o usuário inserir um número positivo.
+
+alert (`Você inseriu o número positivo ${numU}.`); //Retorna o número digitado.
